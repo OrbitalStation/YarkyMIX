ea17eafdeb45625442e0c29136e7d211919f0867
fl1rexchill <wertggg37@mail.ru>
2023-11-11 12:31:15 +0300

    refactor regist algo

--- commands/class_register.py
+++ commands/class_register.py
@@ -4,33 +4,35 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 def class_register(bot, message):
     reply = bot.send_message(message.chat.id, 'Привет! Чтобы у меня была возможность помогать тебе, мне надо '
-                                                 'узнать твоё расписание. Напиши мне своё расписание на понедельник. '
-                                                 'Пример:\nМатематика\nРусский\nФизра\nОБЖ\nАнглийский')
-    bot.register_next_step_handler(reply, weekday([
-        'Воскресенье',
-        'Суббота',
-        'Пятница',
-        'Четверг',
-        'Среда',
-        'Вторник',
-        'Понедельник'
+                                              'узнать твоё расписание. Напиши мне своё расписание на понедельник. '
+                                              'Пример:\nМатематика\nРусский\nФизра\nОБЖ\nАнглийский')
+    bot.register_next_step_handler(reply, weekday(bot, [
+        '',
+        'воскресение',
+        'субботу',
+        'пятницу',
+        'четверг',
+        'среду',
+        'вторник'
     ], []))
 
 
 def weekday(bot, remains: list[str], subs):
-    if len(remains) == 0:
-        return lambda m: finish_weekdays(m, subs)
-    day = remains.pop()
+    if len(remains) != 0:
+        day = remains.pop()
 
     def inner(message):
         subs.extend(message.text.strip().split("\n"))
         reply = bot.send_message(message.chat.id, f'На {day}')
         bot.register_next_step_handler(reply, weekday(bot, remains, subs))
 
+    if len(remains) == 0:
+        return lambda m: finish_weekdays(bot, m, subs)
     return inner
 
 
-def finish_weekdays(bot,message, subs):
+def finish_weekdays(bot, message, subs):
+    subs.extend(message.text.strip().split("\n"))
     print(subs)
     subs = list(set(subs))
     userssub = ";".join(subs)
