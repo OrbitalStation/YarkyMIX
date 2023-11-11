a6c1ee880029c8747854ae967c4572f9f774f0bc
fl1rexchill <wertggg37@mail.ru>
2023-11-11 12:55:07 +0300

    add no lesson button in class_register.py

--- commands/class_register.py
+++ commands/class_register.py
@@ -1,11 +1,17 @@
+from telebot import types
+
 from database import db
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def class_register(bot, message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Нет уроков")
+    markup.add(btn1)
     reply = bot.send_message(message.chat.id, 'Привет! Чтобы у меня была возможность помогать тебе, мне надо '
                                               'узнать твоё расписание. Напиши мне своё расписание на понедельник. '
-                                              'Пример:\nМатематика\nРусский\nФизра\nОБЖ\nАнглийский')
+                                              'Пример:\nМатематика\nРусский\nФизра\nОБЖ\nАнглийский',
+                             reply_markup=markup)
     bot.register_next_step_handler(reply, weekday(bot, [
         '',
         'воскресение',
@@ -22,8 +28,11 @@ def weekday(bot, remains: list[str], subs):
         day = remains.pop()
 
     def inner(message):
-        subs.extend(message.text.strip().split("\n"))
-        reply = bot.send_message(message.chat.id, f'На {day}')
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Нет уроков")
+        markup.add(btn1)
+        _subs_append(message, subs)
+        reply = bot.send_message(message.chat.id, f'На {day}', reply_markup=markup)
         bot.register_next_step_handler(reply, weekday(bot, remains, subs))
 
     if len(remains) == 0:
@@ -32,9 +41,9 @@ def weekday(bot, remains: list[str], subs):
 
 
 def finish_weekdays(bot, message, subs):
-    subs.extend(message.text.strip().split("\n"))
+    _subs_append(message, subs)
     print(subs)
-    subs = list(set(subs))
+    subs = list(set([item.replace(';', ' ') for lit in subs for item in lit]))
     userssub = ";".join(subs)
     db.update_user(message.from_user.id, subjects_str_repr=userssub)
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
@@ -43,3 +52,10 @@ def finish_weekdays(bot, message, subs):
     markup.add(btn1, btn2)
     bot.send_message(message.chat.id, "Поздравляю! Вы заполнили расписание. Теперь можно записывать и "
                                       "просматривать дз", reply_markup=markup)
+
+
+def _subs_append(message, subs):
+    if message.text != "Нет уроков":
+        subs.append(message.text.strip().split("\n"))
+    else:
+        subs.append([])
